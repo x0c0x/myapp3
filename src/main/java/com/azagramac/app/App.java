435afc66a66ffaf31674f34d4eb86e1a17b77b98
Jose <azagramac@gmail.com>
2019-03-11 15:24:43 +0100

    Update App.java

--- src/main/java/com/azagramac/app/App.java
+++ src/main/java/com/azagramac/app/App.java
@@ -1,16 +1,8 @@
 package com.azagramac.app;
 
-import java.security.SecureRandom;
-
 public class App {
-	private static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
-	private static final SecureRandom RANDOM = new SecureRandom();
-
-	public static String generate(int count) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < count; ++i) {
-			sb.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
-		}
-		return sb.toString();
-	}
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
 }
